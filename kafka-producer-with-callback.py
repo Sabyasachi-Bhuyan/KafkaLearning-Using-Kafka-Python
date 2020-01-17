9414d4b67bfa23eeb712b4a6790a0df9604e77ca
Hardik <patirahardik@gmail.com>
2020-01-17 15:50:23 +0530

    Producing more messages

--- kafka-producer-with-callback.py
+++ kafka-producer-with-callback.py
@@ -8,20 +8,22 @@ listBootstrapServer = ['127.0.0.1:9092']
 producer = KafkaProducer(bootstrap_servers=listBootstrapServer,
                          value_serializer=lambda x: dumps(x).encode('utf-8'))
 
-message = 'Testing Kafka'
-
 
 def on_send_success(record_metadata):
     print("Topic : " + record_metadata.topic)
     print("Partition : " + str(record_metadata.partition))
     print("Offset : " + str(record_metadata.offset))
 
+
 # On Error
 def on_send_error(excp):
     logging.error('I am an errback', exc_info=excp)
 
 
-producer.send('Learning_Kafka_1', message).add_callback(on_send_success).add_errback(on_send_error)
+for i in range(10):
+    message = "Hello" + str(i)
+    producer.send('Learning_Kafka_1', message).add_callback(on_send_success).add_errback(on_send_error)
+
 
 # Flush data
 producer.flush()
